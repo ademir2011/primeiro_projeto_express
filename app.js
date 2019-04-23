af9c8e90913067a97bfaf91dc6c45a8041289e69
Ademir Bezerra <ademirbezerra2012@gmail.com>
2019-04-22 22:19:00 -0300

    Alteracoes de deploy

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 
+var port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
-  res.send("Bem vindo!");
+  res.status(201);
+  res.json({
+    nome: "Ademir",
+    linguagem: "javascript"
+  });
 });
 
-const server = app.listen(3000, _ => {
+const server = app.listen(port, _ => {
   var host = server.address().address;
   var port = server.address().port;
   console.log(`Example app listening at http://${host}:${port}`);
